fe9f972ae996e980ad7d60a6cd596d7690eb48e6
HA_GO\antic <nmckinzie@smcm.edu>
2024-12-02 17:41:43 -0600

    fixed bugs

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const inquirer = require('inquirer');
-const fs = require("fs");
+const fs = require('fs');
 const { Square, Circle, Triangle } = require("./lib/shapes.js")
-import createLogo from './lib/logo.js';
 
 const questions = [
     {
@@ -25,25 +24,47 @@ const questions = [
         type: 'list',
         message: 'What color is the shape?',
         name: 'shape color',
+        choices: ["red", "blue", "green", "yellow", "pink", "purple", "orange", "gray", "brown", "black"]
     },
 ];
 
+function generateSVG(response) {
+    let shape;
+    if (response['logo shape'] === "square") {
+        shape = Square(response['shape color']);
+    } else if (response['logo shape'] === "circle") {
+        shape = Circle(response['shape color']);
+    } else if (response['logo shape'] === "triangle") {
+        shape = Triangle(response['shape color']);
+    }
+    return `
+        <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
+        ${shape.render()}
+        <text x="50%" y="50%" font-size="30" fill="${response['text color']}" text-anchor="middle" alignment-baseline="central">${response.text}</text>
+    </svg>
+  `;
+}
 
 
 function enforceThree(response) {
-    return response.text.length > 3
-      ? (function () {
-          throw new Error('Please enter no more than 3 characters');
-        })() 
-      : (generateSVG(response), console.log(`${response.text}.svg created!`));
-  }
+    if (response.text.length > 3) {
+        throw new Error('Please enter no more than 3 characters');
+    } else {
+        const svgContent = generateSVG(response); // Call generateSVG with response
+        console.log(`${response.text}.svg generated`);
+        fs.writeFileSync(`${response.text}.svg`, svgContent); // Save the SVG to a file
+    }
+}
 
-  function init() {
+function init() {
     inquirer
-    .prompt(questions)
-    .then((response) => {
-        enforceThree(response);
-    })
+        .prompt(questions)
+        .then((response) => {
+            enforceThree(response);
+        })
 }
 
 init();
+
+
+module.exports = enforceThree
